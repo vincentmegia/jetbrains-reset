83b674af4c6a56f03eff061db994b40bde52e902
iamstupendous <vincent.megia@gmail.com>
2020-05-21 21:41:14 +0800

    initial commit

---
+++ jetbrains-reset.sh
@@ -0,0 +1,6 @@
+rm -rf ~/Library/Application\ Support/JetBrains/Rider2020.1/eval
+rm -rf ~/Library/Application\ Support/JetBrains/IntelliJIdea2020.1/eval
+
+
+rm -rf ~/Library/Preferences/IntelliJIdea2020.1/options/other.xml
+rm -rf ~/Library/Preferences/Rider2020.1/options/other.xml
