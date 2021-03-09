582c6b1bb3723eda03d409be613bbbd27c070b2d
vincentmegia <vincent.megia@gmail.com>
2021-03-09 11:06:27 +0800

    updated the script

--- jetbrains-reset.sh
+++ jetbrains-reset.sh
@@ -1,6 +1,9 @@
 rm -rf ~/Library/Application\ Support/JetBrains/Rider2020.1/eval
 rm -rf ~/Library/Application\ Support/JetBrains/IntelliJIdea2020.1/eval
-
+rm -rf ~/Library/Application\ Support/JetBrains/Rider2020.2/eval
+rm -rf ~/Library/Application\ Support/JetBrains/Rider2020.3/eval
 
 rm -rf ~/Library/Preferences/IntelliJIdea2020.1/options/other.xml
 rm -rf ~/Library/Preferences/Rider2020.1/options/other.xml
+rm -rf ~/Library/Preferences/Rider2020.2/options/other.xml
+rm -rf ~/Library/Preferences/Rider2020.3/options/other.xml
